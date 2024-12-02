941885575a01614f4c59bd1ae191fe7771e68e2c
GowriShankar77 <162251748+GowriShankar77@users.noreply.github.com>
2024-12-02 09:34:50 +0800

    menu

---
+++ Assignment-2 StudentStatistics/__SHELL2.java
@@ -0,0 +1,10 @@
+
+public class __SHELL2 extends bluej.runtime.Shell {
+public static void run() throws Throwable {
+final bluej.runtime.BJMap __bluej_runtime_scope = getScope("C:\\Users\\gowri\\Desktop\\Units\\Fundamentals of programming\\Assignment 2\\Assignment-2 StudentStatistics");
+final Menu menu1 = (Menu)__bluej_runtime_scope.get("menu1");
+
+
+menu1.displayMenu();
+
+}}
